bb90216d5d3d217738fbbf67d0b1d81a87cdfc80
Jenny Gao <gao.jenny9@gmail.com>
2020-09-08 00:14:11 -0600

    add tests for Form component

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Appointment from '../Appointment/index';
 
+// afterEach(cleanup);
+
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
