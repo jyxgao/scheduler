ff376c6dac8d29d98c770d3954bfb825d28ae4d5
Jenny Gao <gao.jenny9@gmail.com>
2020-09-07 20:16:53 -0600

    created first test for Appointment compoment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,7 +1,8 @@
+// we need React.createElement to render our component
 import React from "react";
-
+// import helper function(s) from react-testing library
 import { render, cleanup } from "@testing-library/react";
-
+// import component we're testing
 import Application from "components/Application";
 
 afterEach(cleanup);
