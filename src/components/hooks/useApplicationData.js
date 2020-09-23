c35ff27c8260cbd1071dffcc962e206cd38bd8c9
Jenny Gao <gao.jenny9@gmail.com>
2020-09-22 20:19:22 -0600

    clean up

--- src/components/hooks/useApplicationData.js
+++ src/components/hooks/useApplicationData.js
@@ -101,21 +101,6 @@ export function useApplicationData() {
       }
     }
   })
-
-  // useEffect(() => {
-	// 	const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-
-	// 	// webSocket.addEventListener('open', event => webSocket.send('ping'));
-	// 	webSocket.addEventListener('message', event => {
-	// 		const newInterview = JSON.parse(event.data);
-	// 		if (newInterview.type === 'SET_INTERVIEW') {
-	// 			dispatch({
-	// 				type: newInterview.type,
-	// 				interview: newInterview.interview,
-	// 				id: newInterview.id,
-	// 			});
-	// 		}
-  // 	});
   
   const setDay = (day) =>
     setState((prev) => ({
